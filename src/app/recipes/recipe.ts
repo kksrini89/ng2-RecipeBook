af7da4e4b8b066bdb2c2b5161e38e96bb4ab920f
Srinivasan <kksrini89@gmail.com>
2016-09-08 01:22:48 +0000

    Initial Commit

---
+++ src/app/recipes/recipe.ts
@@ -0,0 +1,7 @@
+export class Recipe {
+    constructor(public name,public description,public imagePath){
+    }
+    // Name : string ='Dummy';
+    // Description : string ='Dummy';
+    // ImagePath : string = 'http://ersuk.co.uk/ekmps/shops/express2011/images/female-tailor-dummy-with-tripod-base-round-top-511-p.jpg'
+}
