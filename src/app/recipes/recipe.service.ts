d4fcd9ccc8a1d73b82e5fd568fe0a2cc34973e8d
Srinivasan <kksrini89@gmail.com>
2016-09-13 08:59:53 +0000

    shopping list service is added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe';
+import { Ingredient } from '../shared';
 
 @Injectable()
 export class RecipeService {
 
-  private recipes:Recipe[] = [new Recipe('Schnitzel','Very tasty','http://i2.wp.com/jidelna-komarov.cz/wp-content/uploads/2016/03/cropped-Wienerschnitzel.jpg?fit=1640%2C686',[]),
+  private recipes:Recipe[] = [new Recipe('Schnitzel','Very tasty','http://i2.wp.com/jidelna-komarov.cz/wp-content/uploads/2016/03/cropped-Wienerschnitzel.jpg?fit=1640%2C686',[
+    new Ingredient('French Fries',2),
+    new Ingredient('Pork Meat',1)
+    ]),
     new Recipe('Summer Salad','Okayish','http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg',[])
     ];
   constructor() { }
